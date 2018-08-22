dbd233e41ff62cb717ca5ba14bacbeef01ce687d
dm4rnde <29925240+dm4rnde@users.noreply.github.com>
2018-08-22 22:11:24 +0300

    Minor text edits

--- setup-docker.sh
+++ setup-docker.sh
@@ -3,8 +3,8 @@
 # build an image
 docker build -t dm4rnde/flask-app-base-apache2-wsgi-py3-ubuntu:0.1 .
 
-# create a network (isolated env)
+# create network (isolated env)
 docker network create basicflaskapp-netw
 
-# start the container (detach, name, start in isolated network, map the ports)
+# start container (detach, name, start in isolated network, map the ports)
 docker run -d --name bflaskapp --net basicflaskapp-netw -p 8888:80 dm4rnde/flask-app-base-apache2-wsgi-py3-ubuntu:0.1
